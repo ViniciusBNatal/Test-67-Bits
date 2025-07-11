6672359a4032686f0fdca68598e199ac93a75b69
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2025-07-11 14:18:30 -0300

    Files organization + MovementControls

---
+++ Assets/Scripts/Input/MovementControls.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem.OnScreen;
+
+[RequireComponent(typeof(CharacterController))]
+public class MovementControls : MonoBehaviour
+{
+    [SerializeField, Min(0f)] private float _speed;
+    [SerializeField] private Transform _cameraRotation;
+    //[SerializeField] private OnScreenStick _screenStick;
+
+    private CharacterController _characterControler;
+    private Vector2 _movementInput;
+
+    private void Start()
+    {
+        _characterControler = GetComponent<CharacterController>();
+        InputManager.Instance.Inputs.Player.Move.performed += HandleMovePerformed;
+    }
+
+    private void HandleMovePerformed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        _movementInput = obj.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 finalDirection = _movementInput.x * _cameraRotation.right + _movementInput.y * _cameraRotation.forward;
+        finalDirection = new Vector3(finalDirection.x, 0, finalDirection.z);
+        _characterControler.Move(_speed * Time.fixedDeltaTime * finalDirection);
+    }
+}
