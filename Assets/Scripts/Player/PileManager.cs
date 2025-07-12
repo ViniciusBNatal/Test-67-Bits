dc7e65ebfd6d6aaaeb5ba7608a2a7e0a4af6688c
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2025-07-12 16:08:33 -0300

    PileManager pile objects size list

--- Assets/Scripts/Player/PileManager.cs
+++ Assets/Scripts/Player/PileManager.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PileManager : MonoSingleton<PileManager>
 {
-    [SerializeField, Min(0f)] private float _spaceBetweenObjects;
+    [SerializeField] private PileObjectSizeData[] _pileObjectsSizeData;
     [SerializeField] private Transform _pilePivot;
     [SerializeField] private byte _maxPileObjects;
     [SerializeField] private byte _initialMaxPileObjects;
@@ -12,16 +13,35 @@ public class PileManager : MonoSingleton<PileManager>
     //[SerializeField, Min(0f)] private float _maxObjectAngle;
     [SerializeField, Min(0f)] private float _tickFrequency;
 
-    private List<Transform> _pileObjects = new List<Transform>();
+    private List<PileObjectData> _pileObjects = new List<PileObjectData>();
     private byte _currentMaxPileObjects;
     private Vector2 _movementInput;
     private Coroutine _inertiaCoroutine;
+
+    [Serializable]
+    public struct PileObjectSizeData
+    {
+        [PoolType] public int PoolType;
+        [Min(0f)] public float ObjectSize;
+    }
+
+    private struct PileObjectData
+    {
+        public Transform Transform;
+        public PileObjectSizeData ObjectData;
+
+        public PileObjectData(Transform transform, PileObjectSizeData objectdata)
+        {
+            Transform = transform;
+            ObjectData = objectdata;
+        }
+    }
 
     private void Start()
     {
         InputManager.Instance.Inputs.Player.Move.performed += HandleMovePerformed;
         _currentMaxPileObjects = _initialMaxPileObjects;
-    }    
+    }
 
     private IEnumerator InertiaCoroutine()
     {
@@ -32,7 +52,7 @@ public class PileManager : MonoSingleton<PileManager>
             for (int i = 0; i < _pileObjects.Count; i++)
             {
                 //finalRotation = Quaternion.Euler(-_movementInput.normalized * (i + 1));
-                _pileObjects[i].transform.rotation = Quaternion.Lerp(_pileObjects[i].transform.rotation,
+                _pileObjects[i].Transform.rotation = Quaternion.Lerp(_pileObjects[i].Transform.rotation,
                     Quaternion.Euler(-_movementInput * (i + 1)),
                     _inertiaAnimationCurve.Evaluate((i + 1) / _maxPileObjects));
             }
@@ -50,12 +70,36 @@ public class PileManager : MonoSingleton<PileManager>
         if (_pileObjects.Count + 1 <= _currentMaxPileObjects)
         {
             PoolingObject obj = GenericPoolManager.Instance.GetPoolingObject(poolData);
+            _pileObjects.Add(new PileObjectData(obj.transform, GetObjectSize()));
             obj.transform.SetParent(_pilePivot);
-            obj.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+            obj.transform.SetLocalPositionAndRotation(GetObjectPositionInPile(), Quaternion.identity);
             if (_inertiaCoroutine == null)
             {
                 _inertiaCoroutine = StartCoroutine(InertiaCoroutine());
             }
+        }
+
+        Vector3 GetObjectPositionInPile()
+        {
+            if (_pileObjects.Count > 1)
+            {
+                int lastIndex = _pileObjects.Count - 1;
+                return (_pileObjects[lastIndex - 1].ObjectData.ObjectSize + _pileObjects[lastIndex].ObjectData.ObjectSize) / 2 * Vector3.up * _pileObjects.Count;
+            }
+            else return Vector3.zero;
+        }
+
+        PileObjectSizeData GetObjectSize()
+        {
+            for (int i = 0; i < _pileObjectsSizeData.Length; i++)
+            {
+                if (_pileObjectsSizeData[i].PoolType == _pileObjects[^1].ObjectData.PoolType)
+                {
+                    return _pileObjectsSizeData[i];
+                }
+            }
+            Debug.LogWarning($"The Object {poolData.name} is not present in the Object Size list");
+            return new PileObjectSizeData();
         }
     }
 
