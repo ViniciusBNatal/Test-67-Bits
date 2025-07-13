d0cf28b9917ce274908ec8ca3f3aaa8a3196f76f
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2025-07-13 17:43:52 -0300

    Punch anim rotates towards target

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -1,17 +1,23 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
 public class PlayerAnimation : MonoBehaviour
 {
     [SerializeField] private Transform _punchRotationBone;
+    [SerializeField] private int _punchAnimationLayer = 1;
 
     private Animator _animator;
+    private Coroutine _animationEndCoroutine;
+    private Quaternion _initialRotation;
     private static readonly int _isMovingBool = Animator.StringToHash("IsMoving");
     private static readonly int _punchTrigger = Animator.StringToHash("Punch");
+    private static readonly int _puchState = Animator.StringToHash("punch");
 
     private void Awake()
     {
         _animator = GetComponent<Animator>();
+        _initialRotation = _punchRotationBone.localRotation;
         GetComponent<MovementControls>().OnInputUpdate += HandleOnInputUpdate;
         GetComponentInChildren<PunchBehaviour>().OnPunch += HandleOnPunch;
     }
@@ -23,7 +29,30 @@ public class PlayerAnimation : MonoBehaviour
 
     private void HandleOnPunch(Vector3 targetPosition)
     {
-        _punchRotationBone.rotation = new Quaternion(_punchRotationBone.rotation.x, Quaternion.LookRotation(targetPosition - transform.position).y, _punchRotationBone.rotation.z, _punchRotationBone.rotation.w);
+        _punchRotationBone.localRotation = new Quaternion(_punchRotationBone.localRotation.x, Quaternion.LookRotation(targetPosition - transform.position).y, _punchRotationBone.localRotation.z, _punchRotationBone.localRotation.w);
         _animator.SetTrigger(_punchTrigger);
+        if(_animationEndCoroutine != null)
+        {
+            StopCoroutine(_animationEndCoroutine);
+            _animationEndCoroutine = null;
+        }
+        _animationEndCoroutine = StartCoroutine(WaitAnimationEndCoroutine());
+    }
+
+    private IEnumerator WaitAnimationEndCoroutine()
+    {
+        if (_animator.GetCurrentAnimatorStateInfo(_punchAnimationLayer).shortNameHash != _puchState)
+        {
+            while (_animator.GetCurrentAnimatorStateInfo(_punchAnimationLayer).shortNameHash != _puchState)
+            {
+                yield return null;
+            }
+        }
+        while (_animator.GetCurrentAnimatorStateInfo(_punchAnimationLayer).shortNameHash == _puchState)
+        {
+            yield return null;
+        }
+        _punchRotationBone.localRotation = _initialRotation;
+        _animationEndCoroutine = null;
     }
 }
